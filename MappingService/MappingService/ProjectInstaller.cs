a9a8bd5dddc7d9a9db20b4bb636f99998c9b47a0
PrimorConsult <156354014+PrimorConsult@users.noreply.github.com>
2025-09-05 10:34:32 -0300

    Criação do projeto.
    Envio de todos os arquivos.

---
+++ MappingService/MappingService/ProjectInstaller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace MappingService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        private ServiceProcessInstaller _processInstaller;
+        private ServiceInstaller _serviceInstaller;
+        public ProjectInstaller()
+        {
+            //InitializeComponent();
+
+            _processInstaller = new ServiceProcessInstaller();
+            _serviceInstaller = new ServiceInstaller();
+
+            _processInstaller.Account = ServiceAccount.LocalSystem;
+
+            _serviceInstaller.ServiceName = "MappingService";
+            _serviceInstaller.StartType = ServiceStartMode.Automatic;
+            _serviceInstaller.Description = "Serviço de monitoramento de pasta";
+
+            Installers.Add(_processInstaller);
+            Installers.Add(_serviceInstaller);
+        }
+    }
+}
---
+++ MappingService/MappingService/Service1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace MappingService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private FileSystemWatcher _watcher;
+        private const string _folderToWatch = @"C:\Users\sps02\Downloads\PastaTeste";
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                if (!System.Diagnostics.EventLog.SourceExists("MappingService"))
+                {
+                    System.Diagnostics.EventLog.CreateEventSource("MappingService", "Application");
+                }
+
+                _watcher = new FileSystemWatcher
+                {
+                    Path = _folderToWatch,
+                    NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.LastWrite | NotifyFilters.CreationTime,
+                    Filter = "*.*"
+                };
+
+                _watcher.Created += OnCreated;
+                _watcher.Deleted += OnDeleted;
+                _watcher.Renamed += OnRenamed;
+                _watcher.EnableRaisingEvents = true;
+
+                MyLogEvent("Serviço iniciado e monitorando a pasta...");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.EventLog.WriteEntry("MappingService", $"Error in OnStart : {ex.Message}", System.Diagnostics.EventLogEntryType.Error);
+            }
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+           MyLogEvent($"O Arquivo foi renomeado de {e.OldFullPath} to {e.FullPath}");
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            MyLogEvent($"O Arquivo foi deletado: {e.FullPath}");
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            MyLogEvent($"O Arquivo foi criado: {e.FullPath}");
+        }
+
+        protected override void OnStop()
+        {
+            _watcher.EnableRaisingEvents = false;
+            _watcher.Dispose();
+        }
+
+        private void MyLogEvent(string message)
+        {
+            var newFolder = Path.Combine(@"C:\Users\sps02\Downloads\PastaTeste", "Logs");
+            if (!Directory.Exists(newFolder)) {Directory.CreateDirectory(newFolder);}
+
+            var filePath = Path.Combine(newFolder, "LogService.txt");
+            using (var writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - {Environment.MachineName} - : {message}");
+            }
+
+            // Também escreve no Event Viewer
+            System.Diagnostics.EventLog.WriteEntry("MappingService", message, System.Diagnostics.EventLogEntryType.Information);
+        }
+    }
+}
